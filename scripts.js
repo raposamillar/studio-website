4e5f8ab03aaf1fcf75666269fb00e00707bf9881
raposamillar <lisa.raposamillar@gmail.com>
2025-09-15 22:32:35 -0400

    add back-to-top button

--- scripts.js
+++ scripts.js
@@ -261,3 +261,18 @@ document.addEventListener('DOMContentLoaded', function() {
         observer.observe(navMenu, { attributes: true });
     }
 });
+
+    // Back-to-Top Button
+    const backToTopBtn = document.getElementById("backToTop");
+
+    window.onscroll = function() {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        backToTopBtn.style.display = "block";
+    } else {
+        backToTopBtn.style.display = "none";
+    }
+    };
+
+    backToTopBtn.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+});
